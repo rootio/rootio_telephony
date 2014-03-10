683c823bd69b35ffb9fd5d49a28e0a91ce14851a
csik <robotic@gmail.com>
2014-03-10 14:12:52 +0300

    parenthesis

--- news_report.py
+++ news_report.py
@@ -90,7 +90,7 @@ class News(StateMachine):
         if r.get('is_master_'+str(self.episode_id))=='none':
             r.set('is_master_'+str(self.episode_id),self.station.id)
             self.is_master = True
-            logger.info("{} is master for news report".format(str(self))
+            logger.info("{} is master for news report".format(str(self)))
 
         #check soundfile
         import requests
